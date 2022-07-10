541c3853145e31d8a7ca4c55bd2e7f216c19d38e
Andre K <andrek@Andres-MacBook-Pro.local>
2022-07-10 04:25:13 -0400

    1st commit

--- main.js
+++ main.js
@@ -4,6 +4,67 @@
 
 
 
+
+/*1 parameter; object 
+{
+    firstName: string,
+    lastName: string, 
+    userRole; string,
+}
+retruns a boolean
+if statemnt to check if admin or not
+*/
+
+// IsAdmin
+
+function isAdmin(user){
+    if(user.userRole === 'Admin'){
+        return true;
+    } else {
+        return false
+    }
+} 
+
+// getEmail
+
+function getEmail(user){
+    return user.firstName.toLowerCase() + "," + user.lastName.toLowerCase() + "@codeimmersives";
+}
+
+// getPlayListLength
+
+function getPlayListLength (playlist){
+    return playlist.songs.length;;
+}
+
+// getHardestHoemwork
+
+function getHardestHoemwork (arr){
+    let lowScore = 100;
+    let Homework = "";
+
+    for (let i = 0; i < arr.length; i++){
+        const score = arr[i].averageScore
+        if (score < lowScore){
+            lowestScore = score; 
+            Homework = arr[i].name
+        }
+    }
+    return Homework
+
+// createPhonebook
+
+function createPhonebook (numbs, names){
+    let phonebook = {}
+    for (let i = 0; i < names.length; i++){
+        phonebook[names[i]] = numbs[i]
+    }
+}
+    return phonebook;
+}
+
+
+
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
